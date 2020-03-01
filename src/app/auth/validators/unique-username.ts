4e9884ad78f44b9d9f672596eb3003e52264003c
Nelson Foltz <nelsfoltz@gmail.com>
2020-03-01 09:54:08 -0500

    add: error handling on unique-username

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
@@ -8,9 +10,23 @@ export class UniqueUsername implements AsyncValidator {
 
   validate = (control: FormControl) => {
     const { value } = control;
-
-    console.log(this.http);
-
-    return null;
+    return this.http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value
+      })
+      .pipe(
+        map(value => {
+          if (value.available) {
+            return null;
+          }
+        }),
+        catchError(err => {
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true });
+          } else {
+            return of({ noConnection: true });
+          }
+        })
+      );
   };
 }
