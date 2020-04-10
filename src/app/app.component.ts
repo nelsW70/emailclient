b243cb2eefcc706325ae3da9f0175c0114c3c918
Nelson Foltz <nelsfoltz@gmail.com>
2020-04-10 12:38:38 -0400

    add: async pipe for signedin$ behavior subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  signedin = false;
+export class AppComponent {
+  signedin$: BehaviorSubject<boolean>;
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.authService.signedin$.subscribe((signedin) => {
-      this.signedin = signedin;
-    });
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
   }
 }
