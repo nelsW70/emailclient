0e7f2591f05de95b4ebc6e6547f16b4e4dc84a59
Nelson Foltz <nelsfoltz@gmail.com>
2020-04-11 10:53:44 -0400

    add: signin method on auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,6 +22,11 @@ interface SignedinResponse {
   username: string;
 }
 
+interface SigninCredentials {
+  username: string;
+  password: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -64,6 +69,14 @@ export class AuthService {
     return this.http.post(this.rootUrl + '/auth/signout', {}).pipe(
       tap(() => {
         this.signedin$.next(false);
+      })
+    );
+  }
+
+  signin(credentials: SigninCredentials) {
+    return this.http.post(this.rootUrl + '/auth/signin', credentials).pipe(
+      tap(() => {
+        this.signedin$.next(true);
       })
     );
   }
