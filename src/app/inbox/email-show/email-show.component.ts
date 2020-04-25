b4f1a3da504d524c1e14dc5a5eedd896bf205963
Nelson Foltz <nelsfoltz@gmail.com>
2020-04-25 12:56:28 -0400

    add: switchMap to get individual email from service

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { EmailService } from './../email.service';
 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  styleUrls: ['./email-show.component.css'],
 })
 export class EmailShowComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
   ngOnInit() {
-  }
+    // this.route.params.subscribe(({ id }) => {
+    //   this.emailService.getEmail(id).subscribe((email) => {
+    //     console.log(email);
+    //   });
+    // });
 
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe((email) => {
+        console.log(email);
+      });
+  }
 }
