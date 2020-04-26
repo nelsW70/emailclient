716ee2033aa426cbeea321ff50fe6666f70f0f6e
Nelson Foltz <nelsfoltz@gmail.com>
2020-04-26 13:28:04 -0400

    add: building out form for modal

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,7 @@ import { EmailIndexComponent } from './email-index/email-index.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { EmailFormComponent } from './email-form/email-form.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
     EmailShowComponent,
     PlaceholderComponent,
     NotFoundComponent,
+    EmailFormComponent,
   ],
-  imports: [CommonModule, InboxRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    InboxRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class InboxModule {}
